511e1269ec72113fe50b45484ea71e0cff665279
hoooonshub <yhn515@naver.com>
2024-05-07 15:52:04 +0900

    Chore:초기 설정
    디펜던시는 spring web 과 spring jpa만 추가.

---
+++ llmproject/src/main/java/com/hanieum/llmproject/LlmprojectApplication.java
@@ -0,0 +1,13 @@
+package com.hanieum.llmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmprojectApplication.class, args);
+	}
+
+}
---
+++ llmproject/src/test/java/com/hanieum/llmproject/LlmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanieum.llmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
