30e4406f60319f0918ed399b5fa59163f8e52602
hoooonshub <yhn515@naver.com>
2024-05-14 21:23:58 +0900

    Feat: exclude db 설정
    db 연결 없이 스프링 실행시키기 위한 어노테이션 추가

--- llmproject/src/main/java/com/hanieum/llmproject/LlmprojectApplication.java
+++ llmproject/src/main/java/com/hanieum/llmproject/LlmprojectApplication.java
@@ -2,8 +2,10 @@ package com.hanieum.llmproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// (exclude = ?) db 연결하지 않고 사용하기 위한 임시 어노테이션
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LlmprojectApplication {
 
 	public static void main(String[] args) {
