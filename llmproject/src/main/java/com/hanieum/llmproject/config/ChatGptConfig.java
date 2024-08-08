d0141735234e609acea35337ddbf2064dbe3c448
hoooonshub <yhn515@naver.com>
2024-08-08 18:49:09 +0900

    fix: OpenAiService 빈 등록

--- llmproject/src/main/java/com/hanieum/llmproject/config/ChatGptConfig.java
+++ llmproject/src/main/java/com/hanieum/llmproject/config/ChatGptConfig.java
@@ -1,6 +1,11 @@
 package com.hanieum.llmproject.config;
 
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 public class ChatGptConfig {
@@ -13,4 +18,12 @@ public class ChatGptConfig {
     public static final Double TEMPERATURE = 0.6;
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
     public static final String CHAT_URL = "https://api.openai.com/v1/chat/completions";
+
+    @Value("#{'${openai.key}'.trim()}")
+    private String token;
+
+    @Bean
+    public OpenAiService openAiService() {
+        return new OpenAiService(token, Duration.ofSeconds(30L));
+    }
 }
