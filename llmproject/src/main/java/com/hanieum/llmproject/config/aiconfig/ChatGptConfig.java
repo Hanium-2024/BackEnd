1b33e3c4eb9f6bf1919f30cd289c32e5186a8a9d
hoooonshub <yhn515@naver.com>
2024-09-25 14:44:12 +0900

    "refactor: gpt 설정, gptService로 이동"

--- llmproject/src/main/java/com/hanieum/llmproject/config/aiconfig/ChatGptConfig.java
+++ llmproject/src/main/java/com/hanieum/llmproject/config/aiconfig/ChatGptConfig.java
@@ -10,16 +10,6 @@ import com.theokanning.openai.service.OpenAiService;
 
 @Configuration
 public class ChatGptConfig {
-	public static final String AUTHORIZATION = "Authorization";
-	public static final String BEARER = "Bearer ";
-	public static final String CHAT_MODEL = "gpt-4o-mini";
-	public static final Integer MAX_TOKEN = 1000;
-	public static final Boolean STREAM = true;
-	public static final String ROLE = "user";
-	public static final Double TEMPERATURE = 0.6;
-	public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
-	public static final String CHAT_URL = "https://api.openai.com/v1/chat/completions";
-
 	@Value("#{'${openai.key}'.trim()}")
 	private String token;
 
